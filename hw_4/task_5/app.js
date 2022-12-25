131eaf926e8dfee23e8b35f8d5b2a643b8793310
Rostyslav Klyba <rklyba@gmail.com>
2022-12-25 04:58:44 +0200

    Added task 5 skeleton

---
+++ hw_4/task_5/app.js
@@ -0,0 +1,47 @@
+/* 5) Реалізувати логіку підрахунку ціни товару по його варіаціях(шаблон наведений тут, при кліку на колір змінювати ціну товару).
+Придумати ще 2 варіації, відяких буде залежати ціна товару.
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///// Function for creating elements
+// const createTag = (element, elementName) =>
+//   elementName = document.createElement(element);
+ 
+///// Function for adding classes
+// const addClass = (elementName, className) => 
+  // elementName.classList.add(className);
+
+// const divTag = createTag('div', 'container');
+// addClass(divTag, 'container');
+
+
+
+// document.body.append(divTag);
+
+// createTag('div', 'container');
+// addClass(createTag('div', 'container'), 'container');
