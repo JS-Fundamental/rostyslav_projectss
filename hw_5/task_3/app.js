46f2eaf9ebf8cb79e3561c7e00cbfbfc5967acac
Rostyslav Klyba <rklyba@gmail.com>
2022-12-27 22:09:13 +0200

    Completed task 3 of hw_5

--- hw_5/task_3/app.js
+++ hw_5/task_3/app.js
@@ -1,11 +1,82 @@
 /*
 3) Зробити гру МАТЕМАТИК. Є масив питань(3-4 штуки), відображати їх по черзі, і давати можливість користувачеві вибирати 1 варіант відповіді, якщо користувач відповість на всі питання вірно, то очистити вікно сайту і поставити бекграунд фото(будь-яке)
-4) Відобразити акронім SOLID в колонку, типу:
-S
-O
-L
-I
-D
-, при кліку на кожну із літер має відкриватись опис конкретного(на який клікнули) принципу, до того має опис бути прихований.
-
 */
+const card = document.querySelector('.card');
+const question = document.querySelector('.question');
+const aAnswer = document.querySelector('.a-ans');
+const bAnswer = document.querySelector('.b-ans');
+const cAnswer = document.querySelector('.c-ans');
+const dAnswer = document.querySelector('.d-ans');
+const radioInput = document.querySelectorAll('input');
+const button = document.querySelector('.btn');
+const imgTag = document.createElement('img');
+const h1Tag = document.createElement('h1');
+
+let questionCounter = 0;
+let score = 0;
+
+// Function to render questions
+const showQuestion = () => {
+  if (questionCounter >= questionsArr.length) {
+    finishQuiz();
+  } else {
+    radioInput.forEach(answer => {
+      answer.checked = false;
+    });
+
+    question.textContent = questionsArr[questionCounter].question;
+    aAnswer.textContent = questionsArr[questionCounter].a;
+    bAnswer.textContent = questionsArr[questionCounter].b;
+    cAnswer.textContent = questionsArr[questionCounter].c;
+    dAnswer.textContent = questionsArr[questionCounter].d;
+  }
+};
+
+// Function to check if the answer is correct
+function checkQuestion() {
+  radioInput.forEach(answer => {
+    if (answer.checked) {
+      let chosenAnswer = answer.className;
+      if (chosenAnswer[chosenAnswer.length - 1] ==
+        questionsArr[questionCounter].correct) {
+        score++;
+      }
+    }
+  });
+}
+
+// Function to render image if all answers are correct
+const renderImage = () => {
+  card.innerHTML = '';
+  card.classList.remove('card');
+  imgTag.setAttribute('src', './My_code.jpg');
+  imgTag.setAttribute('alt', '100% correct');
+  card.append(imgTag);
+}
+
+// Function to render screen if some answers are incorrect
+renderScreen = () => {
+  card.innerHTML = '';
+  h1Tag.innerText = 
+  `The number of right answers is ${score}. 
+  Go back to school!`;
+  card.append(h1Tag);
+}
+
+button.addEventListener('click', () => {
+  checkQuestion();
+  questionCounter++;
+  showQuestion();
+});
+
+// Start app
+showQuestion();
+
+// Function to end app
+finishQuiz = () => {
+  if (score == questionsArr.length) {
+    renderImage();
+  } else {
+    renderScreen();
+  }
+}
---
+++ hw_5/task_3/data.js
@@ -0,0 +1,37 @@
+const questionsArr = [
+  {
+    question: 'What is Pi equals to (2 decimals)?',
+    a: '7',
+    b: '22',
+    c: '3.14',
+    d: "I don't like math",
+    correct: 'c'
+  },
+
+  {
+    question: 'What is 100 / 5 equals to?',
+    a: '20',
+    b: '0',
+    c: '18',
+    d: '25',
+    correct: 'a'
+  },
+  
+  {
+    question: 'What is 7 * 7 equals to?',
+    a: '33',
+    b: '49',
+    c: '100',
+    d: '10.3',
+    correct: 'b'
+  },
+
+  {
+    question: 'What is 3 + 7 equals to?',
+    a: '11',
+    b: '200',
+    c: '88',
+    d: '10',
+    correct: 'd'
+  }
+];
