5b4e2494a6108d14737dffc6cbc85e25c3836773
Rostyslav Klyba <rklyba@gmail.com>
2022-12-28 12:22:45 +0200

    Completed task 4 of hw_5

--- hw_5/task_4/app.js
+++ hw_5/task_4/app.js
@@ -8,6 +8,29 @@ D
 , при кліку на кожну із літер має відкриватись опис конкретного(на який клікнули) принципу, до того має опис бути прихований.
 */
 
+document.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('toggle')) {
+    return;
+  }
+  
+  let content = document.querySelector(e.target.hash);
+  // e.target.hash gives us the hash value of tag <a>
+  if (!content) {
+    return;
+  }
 
+  e.preventDefault();
+  if (content.classList.contains('active')) {
+    content.classList.remove('active');
+    return;
+  }
+  content.classList.toggle('active');
+});
 
+const manageAccordion = () => {
+  let accordions = document.querySelectorAll('.accordion.active');
+  for (let i = 0; i < accordions.length; i++) {
+    accordions[i].classList.remove('active');
+  }
+}
 
