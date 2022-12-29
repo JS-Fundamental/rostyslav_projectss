15ca32181df42a33ec2afc2841fec14833a5b61c
Rostyslav Klyba <rklyba@gmail.com>
2022-12-29 14:42:37 +0200

    Completed task_5 of HW_5

--- hw_5/task_5/app.js
+++ hw_5/task_5/app.js
@@ -1,4 +1,63 @@
 /*
 5) створити тест, будь-які 5 питань, якщо відповідь на питання правильне, то закрашувати фон в зелений колір, якщо ні, то в червоний і відкривати знизу правильну відповідь та пояснення. Питання відображати по 1 штуку.
 */
+const bodyTag = document.querySelector('body');
+const questionDiv = document.querySelector('.question');
+const inputTag = document.querySelector('input');
+const answerTag = document.querySelector('.rightAnswer')
+const buttonTag = document.querySelector('.btn');
+let counter = 0;
 
+buttonTag.addEventListener('click', () => {
+  inputTag.value = '';
+  counter++;
+  removeClass();
+  feedQuestion();
+  inputTag.focus();
+});
+
+inputTag.addEventListener('change', (event) => {
+  addClass(event);
+});
+
+// Function to add classes
+const addClass = (event) => {
+  if (event.target.value == rightAnswers[counter]) {
+    bodyTag.classList.add('greenBackground');
+  } else {
+    bodyTag.classList.add('redBackground');
+    answerTag.innerHTML = rightAnswers[counter];
+  }
+};
+
+// Function to remove classes
+const removeClass = () => {
+  if (bodyTag.classList.contains('greenBackground')) {
+    bodyTag.classList.remove('greenBackground');
+  } else if (bodyTag.classList.contains('redBackground')) {
+    bodyTag.classList.remove('redBackground');
+  }
+}
+
+// Function to handle questions
+const feedQuestion = () => {
+  answerTag.innerHTML = '';
+  questionDiv.innerHTML = '';
+  document.body.classList.remove('redBackground');
+  questionDiv.append(questions[counter]);
+  
+  if (counter >= questions.length) {
+    clearDom();  
+    document.body.innerText = 'End of Quiz';
+  }
+};
+
+// Function to clear DOM when the quiz is over
+const clearDom = () => {
+  questionDiv.remove();
+  inputTag.remove();
+  buttonTag.remove();
+};
+
+// Start the app 
+feedQuestion();
---
+++ hw_5/task_5/data.js
@@ -0,0 +1,16 @@
+const questions = [
+  'What is the capital of Great Britain?',
+  'What is the name of our planet?',
+  'Where is the Eiffel tower located?',
+  'In what country is the Chernobyl nuclear plant located ?',
+  'How long is an Olympic swimming pool (in meters)?'
+];
+
+const rightAnswers = [
+  'London',
+  'Earth',
+  'Paris',
+  'Ukraine',
+  '50'
+];
+
