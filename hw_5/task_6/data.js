30204239a1e0adcd42cc8d304756a8a4f8026a3c
Rostyslav Klyba <rklyba@gmail.com>
2023-01-01 09:29:44 +0200

    Did the sketch of the app(html + css)

---
+++ hw_5/task_6/data.js
@@ -0,0 +1,24 @@
+const questions = [
+  'What are made and repaired by a cobbler;',
+  'Which city will host the 2028 Olympic Games?',
+  'What is the smallest country in the world?',
+  'In what year was the first iPhone released?',
+  'What colour were the Pyramids of Giza originally?'
+];
+
+const answers = [
+  ['Shoes', 'Roads', 'Windows', 'Jewellery'],
+  ['Beijing', 'Paris', 'Tokyo', 'Los Angeles'],
+  ['Iceland', 'Seychelles', 'Spain', 'Vatican City'],
+  ['2000', '2004', '2007', '2009'],
+  ['Orange', 'Blue', 'Pink', 'White']
+];
+
+const correctAns = [
+  'Shoes', 
+  'Los Angeles', 
+  'Vatican City', 
+  '2007', 
+  'White'];
+
+// 64 125 250 500 1 000
