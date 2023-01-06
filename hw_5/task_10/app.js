572658ddca2d1a0cc0f4ce85d763d3dd2744f263
Rostyslav Klyba <rklyba@gmail.com>
2023-01-06 09:29:45 +0200

    Added logic to delete/add items to the list

--- hw_5/task_10/app.js
+++ hw_5/task_10/app.js
@@ -1,6 +1,20 @@
 /* 
   10) зробити список, після останього елементу списку має бути поле(інпут) та кнопка щоб додати новий елемент списку. Біля кожного елементу списку має бути знак - який видаляє даний елемент списку. Список зберігати в масиві.
 */
+const ulTag = document.querySelector('ul');
+const spanTags = [...document.querySelectorAll('span')];
+const buttonTag = document.querySelector('.btn');
 
+// Adding items to the list
+buttonTag.addEventListener('click', () => {
+  ulTag.insertAdjacentHTML('beforeend', `<li>${toDoList[1]}<span>x</span></li>`);
+});
+
+
+// Removing items from the list
+spanTags.forEach(tag => 
+  tag.addEventListener('click', (e) => {
+    e.target.parentElement.remove();
+}));
 
 
---
+++ hw_5/task_10/data.js
@@ -0,0 +1,3 @@
+const toDoList = [
+  'Shopping',
+  'Clean house'];
